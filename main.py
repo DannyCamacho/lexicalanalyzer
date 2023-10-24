16ade9b2775916c1bed659d8367cd6740d88af52
Daniel Camacho <dcamacho4@saddleback.edu>
2023-10-24 15:36:04 -0700

    add lexeme_list.txt and token functionality to parse() and main()

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 #! /usr/bin/env python3
 
-import re
+import re  # import regular expressions
 
 
-def read_items(filename):
-    item_list = []
-    with open(filename) as file:
-        for line in file:
-            item_list.append(line)
-    return item_list
+def read_items(filename):  # read_items() function
+    item_list = []  # list to store each line of txt file
+    with open(filename) as file:  # open file for reading
+        for line in file:  # for loop to access each line of txt file
+            item_list.append(line)  # append line to item_list
+    return item_list  # return item_list
 
 
-def parse(code):
-    tokens = []  # list to store tuples of (lexeme, token)
+def parse(code):  # parse() function
+    lexeme_list = []  # list to store tuples of (lexeme, token)
     for line in code:  # for loop to grab each line of code individually
         line = re.split("//", line)[0]  # removes comments by taking first part of split
         line = re.sub("^\s+|\s+$", "", line)  # removes leading/trailing whitespace
         line = re.split('\s+', line)  # splits line up by whitespace
         for item in line:  # for loop to grab each item within the line of code
-            lexemes = re.findall("[+]", item)
-            for lexeme in lexemes:
-                tokens.append(lexeme)
-    return tokens
+            lexemes = re.split("[\W]", item)  # split at non-word characters
+            lexemes += re.findall("[\W]", item)  # find all non-word characters
+            lexemes = list(filter(None, lexemes))  # remove empty items from list
+            for lexeme in lexemes:  # for loop to access each lexeme
+                lexeme_list.append(lexeme)  # append lexeme to lexemes
+    return lexeme_list  # return lexeme_list
 
 
-def main():
+def main():  # main() function
     code = read_items("code.txt")
-    tokens = parse(code)
-    for token in tokens:
-        print(f"\"{token}\" = {token}")
+    token_list = read_items("lexeme_list.txt")
+    token_dict = {}
+    for token in token_list:
+        token = token.split(',')
+        token_dict[token[0]] = token[1].strip()
+    lexemes = parse(code)
+    for lexeme in lexemes:
+        if lexeme in token_dict:
+            print(f"\"{lexeme}\" = {token_dict[lexeme]}")
+        elif re.match(r'^([\s\d]+)$', lexeme):
+            print(f"\"{lexeme}\" = {'integer'}")
+        else:
+            print(f"\"{lexeme}\" = {'identifier'}")
 
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__":  # if we are in current file
+    main()  # run main from current file
