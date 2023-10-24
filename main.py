f6ddf2f2d9bd579124ed2a93528371eacff2d0d4
Daniel Camacho <newsypaper@gmail.com>
2023-10-24 13:42:09 -0700

    add whitespace cleanup functionality to parse()

--- main.py
+++ main.py
@@ -7,16 +7,20 @@ def read_items(filename):
     item_list = []
     with open(filename) as file:
         for line in file:
-            line = line.replace("\n", "")
             item_list.append(line)
     return item_list
 
 
 def parse(code):
-    tokens = []
-    for line in code:
-        line = line.split("//")[0]
-        print(line)
+    tokens = []  # list to store tuples of (lexeme, token)
+    for line in code:  # for loop to grab each line of code individually
+        line = re.split("//", line)[0]  # removes comments by taking first part of split
+        line = re.sub("^\s+|\s+$", "", line)  # removes leading/trailing whitespace
+        line = re.split('\s+', line)  # splits line up by whitespace
+        for item in line:  # for loop to grab each item within the line of code
+            lexemes = re.findall("[+]", item)
+            for lexeme in lexemes:
+                tokens.append(lexeme)
     return tokens
 
 
@@ -24,7 +28,7 @@ def main():
     code = read_items("code.txt")
     tokens = parse(code)
     for token in tokens:
-        print(f"\"{token[0]}\" = {token[1]}")
+        print(f"\"{token}\" = {token}")
 
 
 if __name__ == "__main__":
