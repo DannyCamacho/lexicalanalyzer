b4b80f8681a1bfe8894893f550628b4c49094ded
Daniel Camacho <newsypaper@gmail.com>
2023-10-26 13:30:11 -0700

    move token_dict building to separate function

--- main.py
+++ main.py
@@ -11,9 +11,17 @@ def read_items(filename):  # read_items() function
     return item_list  # return item_list
 
 
-def parse(code):  # parse() function
+def build_dict(token_list):
+    token_dict = {}  # dictionary to store lexeme-token pairs
+    for token in token_list:  # for loop to add each lexeme-token combination
+        token = token.split(' ')  # split at comma
+        token_dict[token[0]] = token[1].strip()  # add to dictionary and strip whitespace
+    return token_dict  # return token_dict
+
+
+def parse(code_list):  # parse() function
     lexeme_list = []  # list to store tuples of (lexeme, token)
-    for line in code:  # for loop to grab each line of code individually
+    for line in code_list:  # for loop to grab each line of code individually
         line = re.split("//", line)[0]  # removes comments by taking first part of split
         line = re.sub("^\s+|\s+$", "", line)  # removes leading/trailing whitespace
         line = re.split('\s+', line)  # splits line up by inner whitespace
@@ -26,14 +34,9 @@ def parse(code):  # parse() function
 
 
 def main():  # main() function
-    code = read_items("code.txt")  # read code from file and store each line into list
-    token_list = read_items("lexeme_list.txt")  # read lexeme-token combinations into list
-    token_dict = {}  # declare token dictionary
-    for token in token_list:  # for loop to add each lexeme-token combination
-        token = token.split(',')  # split at comma
-        token_dict[token[0]] = token[1].strip()  # add to dictionary and strip whitespace
-    lexemes = parse(code)  # get lexeme list from parse(code)
-    for lexeme in lexemes:  # for loop to access each lexeme in the lexeme list
+    token_dict = build_dict(read_items("lexeme_list.txt"))  # build dict from lexeme_list.txt
+    lexeme_list = parse(read_items("code.txt"))  # get lexeme list from code.txt
+    for lexeme in lexeme_list:  # for loop to access each lexeme in the lexeme list
         if lexeme in token_dict:  # if the lexeme exists in the dictionary
             print(f"\"{lexeme}\" = {token_dict[lexeme]}")  # print the lexeme-token combination
         elif re.match(r'^([\s\d]+)$', lexeme):  # else if current lexeme is made up of only digits
