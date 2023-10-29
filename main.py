12ca3fa3bac0ede9965170145b926ac19618966d
Daniel Camacho <dcamacho4@saddleback.edu>
2023-10-29 14:28:10 -0700

    comments added and code adjusted

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import re  # import regular expressions
 
 
-def read_items(filename):  # read_items() function
+def read_file(filename):  # read_file() function: read from file and store into list
     item_list = []  # list to store each line of txt file
     with open(filename) as file:  # open file for reading
         for line in file:  # for loop to access each line of txt file
@@ -11,35 +11,33 @@ def read_items(filename):  # read_items() function
     return item_list  # return item_list
 
 
-def build_dict(token_list):
+def build_dict(token_list):  # build_dict() function: build dictionary from given list
     token_dict = {}  # dictionary to store lexeme-token pairs
     for token in token_list:  # for loop to add each lexeme-token combination
-        token = token.split(' ')  # split at comma
-        token_dict[token[0]] = token[1].strip()  # add to dictionary and strip whitespace
+        token = re.split('\s+', token)  # split at whitespace
+        token_dict[token[0]] = token[1]  # add to dictionary and strip whitespace
     return token_dict  # return token_dict
 
 
-def parse(code_list):  # parse() function
+def parse(code_list):  # parse() function: traverse list to separate into individual lexemes
     lexeme_list = []  # list to store tuples of (lexeme, token)
-    for line in code_list:  # for loop to grab each line of code individually
-        line = re.split("//", line)[0]  # removes comments by taking first part of split
-        line = re.sub("^\s+|\s+$", "", line)  # removes leading/trailing whitespace
-        line = re.split('\s+', line)  # splits line up by inner whitespace
-        for item in line:  # for loop to grab each item within the line of code
+    for line_of_code in code_list:  # for loop to grab each line_of_code of code individually
+        line_of_code = re.split("//", line_of_code)  # splits line_of_code up at // (comment)
+        line_of_code = re.split('\s+', line_of_code[0])  # splits first part of line_of_code up by whitespace
+        for item in line_of_code:  # for loop to grab each item within the line_of_code of code
             lexemes = re.split("(\W)", item)  # split at non-word characters
-            lexemes = list(filter(None, lexemes))  # remove empty items from list
-            for lexeme in lexemes:  # for loop to access each lexeme
+            for lexeme in list(filter(None, lexemes)):  # for loop to access each non-empty element of lexemes
                 lexeme_list.append(lexeme)  # append lexeme to lexemes
     return lexeme_list  # return lexeme_list
 
 
 def main():  # main() function
-    token_dict = build_dict(read_items("lexeme_list.txt"))  # build dict from lexeme_list.txt
-    lexeme_list = parse(read_items("code.txt"))  # get lexeme list from code.txt
+    token_dict = build_dict(read_file("lexeme_token_pairs.txt"))  # build dict from lexeme_token_pairs.txt
+    lexeme_list = parse(read_file("code.txt"))  # get lexeme list from code.txt
     for lexeme in lexeme_list:  # for loop to access each lexeme in the lexeme list
         if lexeme in token_dict:  # if the lexeme exists in the dictionary
             print(f"\"{lexeme}\" = {token_dict[lexeme]}")  # print the lexeme-token combination
-        elif re.match(r'^([\s\d]+)$', lexeme):  # else if current lexeme is made up of only digits
+        elif re.match(r'^([\d]+)$', lexeme):  # else if current lexeme is made up of only digits
             print(f"\"{lexeme}\" = {'integer'}")  # print the lexeme as integer
         elif re.match(r'([^\W]+)$', lexeme):  # else if current lexeme is made up of word characters
             print(f"\"{lexeme}\" = {'identifier'}")  # print the lexeme as identifier
